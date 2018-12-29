215ba1ed1c8b7ed52cc0d20d8bf264e9867ed1c0
ifunebe <yang.daniel2000@gmail.com>
2018-12-28 19:22:12 -0500

    SecretSantalesgitit

---
+++ secretSanta.py
@@ -0,0 +1,33 @@
+import json
+from pymongo import MongoClient
+from bson import ObjectId
+from flask import Flask, request
+app = Flask(__name__)
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+santa_db = client['santa-db']
+
+users = []
+
+@app.route("/users", methods=["POST", "GET"])
+def user_list():
+    if request.method == 'GET':
+        user_coll = santa_db['user']
+        print(list(users_coll.find()))
+        return JSONEncoder().encode(list(user_coll.find()))
+    else:
+        user = {
+            "UserName": request.get_json()['UserName'],
+            "Name": request.get_json()['Name'],
+            "Password": request.get_json()['Password'],
+            "Gift": request.get_json()['Gift']
+        }
+        user_coll = santa_db['user']
+        user_coll.insert_one(usr)
+pygobject==3.26.1
+        return "FINSHED INSERTION."
