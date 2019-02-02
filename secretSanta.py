44543810aa57da7b5c126ee8f88c098ef1eb4467
ifunebe <yang.daniel2000@gmail.com>
2019-02-01 19:01:05 -0500

    Fucking Finally

--- secretSanta.py
+++ secretSanta.py
@@ -4,7 +4,7 @@ from bson import ObjectId
 from flask import *
 app = Flask(__name__)
 app.secret_key= 'rutgersIsADeadMeme'
-
+SESSION_TYPE = 'redis'
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
@@ -25,16 +25,11 @@ def send_js(path):
 def main_page():
     return render_template("index.html")
 
-@app.route("/logout/<Email>", methods=["GET"])
-def logout(Email):
-    user = santa['user']
-    user.update({"Email": Email},{"$set":{"Logged_in": False}})
-    flash("You have been logged out!")
-    return redirect(url_for('login'))
-
 @app.route("/login", methods=["POST", "GET"])
 def login():
     error = None
+    if request.method == 'GET':
+        return render_template('login.html', error=error)
     if request.method == 'POST':
         user = santa['user']
         email = request.form['Email']
@@ -43,14 +38,8 @@ def login():
         if loginUser is None:
             error = "Invalid Credentials, Make sure you are registered and then try again."
         else:
-            loginUser = user.find_one({'Email' : email, 'Password': pw, 'Logged_in' : True})
-            if loginUser:
-                flash("You are already logged in!")
-                return redirect(url_for('dashboard', Email=email))
-            else:
-                user.update({"Email": email},{"$set":{"Logged_in": True}})
-                flash("You have been logged in!")
-                return redirect(url_for('dashboard', Email=email))
+            session['Email'] = email
+            return redirect(url_for('dashboard', Email=email))
     print(error)
     return render_template('login.html', error=error)
 
@@ -66,7 +55,6 @@ def register():
                 "Name": request.form['Name'],
                 "Password": request.form['Password'],
                 "Gift": request.form['Gift'],
-                "Logged_in": True
             }
             user_coll = santa['user']
             user_coll.insert_one(usr)
@@ -79,30 +67,21 @@ def register():
         print(list(user.find()))
         return render_template('register.html')
 
-@app.route("/dashboard", methods=["GET"])
-def get_dashboard():
-    email = request.args.get('Email')
-    user = santa['user']
-
-    loginUser = user.find_one({'Email': email})
-
-    # if loginUser:
-    #     not_logged_in = user.find_one({'Email': email, 'Logged_in': False})
-    #     if not_logged_in:
-    #         flash("You are not logged in")
-    #         return redirect(url_for('login'))
-    return render_template('dashboard.html')
-
-    # return redirect(url_for('main_page'))
-
-@app.route("/dashboard", methods=["DELETE", "POST"])
+@app.route("/dashboard/<Email>", methods=["GET", "POST", "DELETE"])
 def dashboard(Email):
-    if request.method == "DELETE":
+    if request.method == "GET":
+        if Email not in session['Email']:
+            flash('Login First!')
+            return redirect(url_for('login'))
+        return render_template('dashboard.html')
+    elif request.method == "DELETE":
         user = santa['user']
         user.delete_one({"Email": Email})
         return 'done'
     elif request.method == "POST":
-        return redirect(url_for('logout', Email=Email))
+        print("aaa")
+        session.pop('Email', None)
+        return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=False)
